0697b3874110229b44f2c6cfa7c963bb149098f5
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2022-06-28 19:37:27 -0300

    adapted to cyclic

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const express = require('express');
+
 // Dependencies imports
 require('dotenv').config();
 
-// Own imports
-const Server = require('./models/server');
+const app = express()
 
-// run server
-const server = new Server();
-server.listen();
+//routes
+const basePathV1 = '/api/v1';
+const authPathV1 = `${basePathV1}/auth`;
+const usersPathV1 = `${basePathV1}/users`;
+app.use('/api/test', require('./routes/test-conn.routes'));
+app.use(authPathV1, require('./routes/auth.routes'));
+app.use(usersPathV1, require('./routes/users.routes'));
+
+app.listen(process.env.PORT || 3000)
